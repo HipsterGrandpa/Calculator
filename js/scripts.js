bc1d39e371ea97989cc3c57dac1b064f81194d70
HipsterGrandpa <conlin.kyle@gmail.com>
2018-12-17 09:40:27 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter A Number"));
+var number2 = parseInt(prompt("Enter Another Number"));
+
+alert(add(number1, number2));
