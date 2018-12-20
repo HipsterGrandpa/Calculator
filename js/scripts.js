2b0ff6884ca136493e89689e763fa16d996bae57
HipsterGrandpa <conlin.kyle@gmail.com>
2018-12-20 11:05:34 -0800

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
+//Back-End
 var add = function(number1, number2) {
-return number1 + number2;
+  return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter A Number"));
-var number2 = parseInt(prompt("Enter Another Number"));
+var subract = function(number1,number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1,number2){
+  return number1 * number2;
+};
+
+var divide = function(number1,number2){
+  return number1 / number2;
+};
+//Front-End
+$(document).ready(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+});
