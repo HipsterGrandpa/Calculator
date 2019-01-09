18118bd3dd30d7f51134a467e76a4e591d582c4e
HipsterGrandpa <conlin.kyle@gmail.com>
2019-01-09 09:55:18 -0800

    final commit?

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,21 @@ var divide = function(number1,number2){
 };
 //Front-End
 $(document).ready(function() {
-  $("form#add").submit(function(event){
+  $("form#calculator").submit(function(event){
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    alert(add(number1, number2));
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result = add(number1, number2);
+    if (operator === "add"){
+      result = add(number1,number2);
+    } else if (operator === "subtract"){
+      result = subtract(number1,number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1,number2);
+    } else if (operator ==="divide") {
+      result = divide(number1,number2);
+    }
+    $("#output").text(result);
   });
 });
